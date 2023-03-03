e634b1f98eb20f1c216360984ec1db4ac326cbf6
pmg <42037161+MINGYU1995@users.noreply.github.com>
2023-03-03 17:22:54 +0900

    Add files via upload

---
+++ bbs/src/main/java/com/bit/controller/InsertController.java
@@ -0,0 +1,34 @@
+package com.bit.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bit.model.TestDao;
+
+@WebServlet("/insert.do")
+public class InsertController extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+	    resp.setHeader("Access-Control-Allow-Credentials", "true");
+		String name=req.getParameter("name");
+		String title=req.getParameter("title");
+		String content=req.getParameter("content");
+		
+		TestDao dao=new TestDao();
+		try {
+			dao.insertOne(name, title, content);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		resp.sendRedirect("index.do");
+	}
+}
